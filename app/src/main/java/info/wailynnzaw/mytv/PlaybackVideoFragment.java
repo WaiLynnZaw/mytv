7f54e2e62bba18629ef2cd7249a5e2e875b660b7
Wai Lynn Zaw <wailynnzaw.lamz@gmail.com>
2021-06-08 17:05:11 +0630

    fix background

--- app/src/main/java/info/wailynnzaw/mytv/PlaybackVideoFragment.java
+++ app/src/main/java/info/wailynnzaw/mytv/PlaybackVideoFragment.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.media.MediaMetadataRetriever;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.leanback.app.VideoSupportFragment;
 import androidx.leanback.app.VideoSupportFragmentGlueHost;
@@ -20,11 +23,15 @@ import java.util.HashMap;
 public class PlaybackVideoFragment extends VideoSupportFragment {
 
     private PlaybackTransportControlGlue<MediaPlayerAdapter> mTransportControlGlue;
-
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = super.onCreateView(inflater, container, savedInstanceState);
+        root.setBackgroundResource(android.R.color.black);
+        return root;
+    }
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         final Movie movie =
                 (Movie) getActivity().getIntent().getSerializableExtra(DetailsActivity.MOVIE);
 
